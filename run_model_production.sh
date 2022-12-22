3d2ef1bbcf8e54583ec28938b173f6418a750456
igor <igorvgp>
2022-12-22 12:31:51 -0300

    changed run model file

--- run_model_production.sh
+++ run_model_production.sh
@@ -5,4 +5,4 @@ data=$(date +'%Y-%m-%dT%H:%M:%S')
 path='/home/ubuntu/DS-clustering-ecommerce-insiders'
 path_to_envs='/home/ubuntu/.pyenv/versions/3.9.13/envs/pa005insidersclustering/bin'
 
-$path_to_envs/papermill $path/Deploy/src/models/c09_deploy_planning.ipynb $path/reports/c09_deploy_$data.ipynb
+$path_to_envs/papermill $path/Deploy/src/models/c09_deploy_planning.ipynb $path/Deploy/reports/c09_deploy_$data.ipynb
